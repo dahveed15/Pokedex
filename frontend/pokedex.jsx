cd916c3de24d3dcfe7c365f5968c52c4c894aa6f
David Harris <dahveed15@gmail.com>
2018-05-10 15:16:26 -0700

    entry file renders the root component

--- frontend/pokedex.jsx
+++ frontend/pokedex.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { fetchAllPokemon } from './util/api_util';
-import { receiveAllPokemon, requestAllPokemon } from './actions/pokemon_actions';
-import { selectAllPokemon } from './reducers/selectors';
+// import { fetchAllPokemon } from './util/api_util';
+// import { receiveAllPokemon, requestAllPokemon } from './actions/pokemon_actions';
+// import { selectAllPokemon } from './reducers/selectors';
 import configureStore from './store/store';
+import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
   const rootEl = document.getElementById('root');
   let store = configureStore();
-  window.store = store;
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
-  window.fetchAllPokemon = fetchAllPokemon;
-  window.receiveAllPokemon = receiveAllPokemon;
-  window.requestAllPokemon = requestAllPokemon;
-  window.selectAllPokemon = selectAllPokemon;
-  ReactDOM.render(<h1>Pokedex</h1>, rootEl);
+  // window.store = store;
+  // window.getState = store.getState;
+  // window.dispatch = store.dispatch;
+  // window.fetchAllPokemon = fetchAllPokemon;
+  // window.receiveAllPokemon = receiveAllPokemon;
+  // window.requestAllPokemon = requestAllPokemon;
+  // window.selectAllPokemon = selectAllPokemon;
+  ReactDOM.render(<Root store={store}/>, rootEl);
 });
