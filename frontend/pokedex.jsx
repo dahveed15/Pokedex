69aab8b0b99b0b2fb2671f3a4ea85c9854a10f3d
David Harris <dahveed15@gmail.com>
2018-05-10 13:27:54 -0700

    store set up

--- frontend/pokedex.jsx
+++ frontend/pokedex.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { fetchAllPokemon } from './util/api_util';
 import { receiveAllPokemon } from './actions/pokemon_actions';
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
   const rootEl = document.getElementById('root');
+  let store = configureStore();
   ReactDOM.render(<h1>Pokedex</h1>, rootEl);
 });
