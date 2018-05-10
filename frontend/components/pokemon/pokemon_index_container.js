dd268088acf2e2a6f97e93f1a25e0daf5a23fa53
David Harris <dahveed15@gmail.com>
2018-05-10 16:26:58 -0700

    pokemon index container complete

---
+++ frontend/components/pokemon/pokemon_index_container.js
@@ -0,0 +1,18 @@
+import { connect } from 'react-redux';
+import { selectAllPokemon } from '../../reducers/selectors';
+import { requestAllPokemon } from '../../actions/pokemon_actions';
+import PokemonIndex from './pokemon_index';
+
+const mapStateToProps = state => ({
+  //empty array or an array of pokemon objects
+  pokemon: selectAllPokemon(state)
+});
+
+const mapDispatchToProps = dispatch => ({
+  requestAllPokemon: () => dispatch(requestAllPokemon())
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(PokemonIndex);
