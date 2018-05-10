b46b0974bbc3c223b3c9ab7d60bf63984f257425
David Harris <dahveed15@gmail.com>
2018-05-10 14:58:07 -0700

    finally moving on to react components

--- frontend/pokedex.jsx
+++ frontend/pokedex.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { fetchAllPokemon } from './util/api_util';
-import { receiveAllPokemon } from './actions/pokemon_actions';
+import { receiveAllPokemon, requestAllPokemon } from './actions/pokemon_actions';
+import { selectAllPokemon } from './reducers/selectors';
 import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
   const rootEl = document.getElementById('root');
   let store = configureStore();
+  window.store = store;
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
+  window.fetchAllPokemon = fetchAllPokemon;
+  window.receiveAllPokemon = receiveAllPokemon;
+  window.requestAllPokemon = requestAllPokemon;
+  window.selectAllPokemon = selectAllPokemon;
   ReactDOM.render(<h1>Pokedex</h1>, rootEl);
 });
