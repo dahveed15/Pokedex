dead7d5ecc0382c544fccfa639c72db2b0a73844
David Harris <dahveed15@gmail.com>
2018-05-10 10:48:08 -0700

    create entities reducer

--- frontend/reducers/pokemon_reducer.js
+++ frontend/reducers/pokemon_reducer.js
@@ -2,8 +2,8 @@ import { RECEIVE_ALL_POKEMON } from '../actions/pokemon_actions';
 import merge from 'lodash/merge';
 
 const pokemonReducer = (state = {}, action) => {
-  //raise error if state attempts to get mutated
   console.log('pokemon reducer');
+  //raise error if state attempts to get mutated
   Object.freeze(state);
   switch (action.type) {
     case RECEIVE_ALL_POKEMON:
