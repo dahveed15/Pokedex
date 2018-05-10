719db21d63837b9e6a48a56727f5e83092947c7f
David Harris <dahveed15@gmail.com>
2018-05-10 16:50:27 -0700

    completed pokemon index component

--- frontend/components/pokemon/pokemon_index.jsx
+++ frontend/components/pokemon/pokemon_index.jsx
@@ -2,7 +2,26 @@ import React from 'react';
 
 class PokemonIndex extends React.Component {
 
+  componentDidMount() {
+    this.props.requestAllPokemon();
+  }
 
+  render() {
+    return (
+      <div>
+        <ul>
+          {
+            this.props.pokemon.map((poke) =>
+                <div>
+                  <li>{poke.name}</li>
+                  <li><img src={poke.img_url} /></li>
+                </div>
+            )
+          }
+        </ul>
+      </div>
+    );
+  }
 
 }
 
