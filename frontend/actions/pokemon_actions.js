7857e6d9f371919e47c8e4e3d96704c98c52d93f
David Harris <dahveed15@gmail.com>
2018-05-10 09:52:11 -0700

    add pokemon reducer file

--- frontend/actions/pokemon_actions.js
+++ frontend/actions/pokemon_actions.js
@@ -1,6 +1,9 @@
 export const RECEIVE_ALL_POKEMON = 'RECEIVE_ALL_POKEMON';
 
-export const receiveAllPokemon = pokemon => ({
-  type: RECEIVE_ALL_POKEMON,
-  pokemon
-});
+export const receiveAllPokemon = pokemon => {
+  console.log('receiveAllPokemon action');
+  return {
+    type: RECEIVE_ALL_POKEMON,
+    pokemon
+  };
+};
--- frontend/util/api_util.js
+++ frontend/util/api_util.js
@@ -1,4 +1,5 @@
 export const fetchAllPokemon = () => {
+  console.log('api utils fetchAllPokemon');
   return $.ajax({
     method: 'GET',
     url: 'api/pokemon'
