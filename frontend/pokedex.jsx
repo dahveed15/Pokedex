675c350e1b2d71ca8a0a89dcb1285931b7657007
David Harris <dahveed15@gmail.com>
2018-05-09 23:09:49 -0700

    pokemon index received in frontend

--- frontend/pokedex.jsx
+++ frontend/pokedex.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { fetchAllPokemon } from './util/api_util';
+import { receiveAllPokemon } from './actions/pokemon_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
   const rootEl = document.getElementById('root');
