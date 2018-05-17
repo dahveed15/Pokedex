40daa9192be4136edd74196cda03553c3427289b
David Harris <dahveed15@gmail.com>
2018-05-16 20:26:53 -0700

    add items reducer logic

--- frontend/reducers/items_reducer.js
+++ frontend/reducers/items_reducer.js
@@ -1,1 +1,17 @@
 import { RECEIVE_POKEMON } from '../actions/pokemon_actions';
+import merge from 'lodash/merge';
+
+const itemsReducer = (state = {}, action) => {
+  console.log('items reducer');
+
+  Object.freeze(state);
+
+  switch (action.type) {
+    case RECEIVE_POKEMON:
+      return merge({}, state, action.pokemon.items);
+    default:
+      return state;
+  }
+};
+
+export default itemsReducer;
