d5ced5bc0788038fe292fa78ba01c201cbc35551
David Harris <dahveed15@gmail.com>
2018-05-16 19:40:13 -0700

    add util for fetching one pokemon

--- frontend/util/api_util.js
+++ frontend/util/api_util.js
@@ -5,3 +5,10 @@ export const fetchAllPokemon = () => {
     url: 'api/pokemon'
   });
 };
+
+export const fetchPokemon = (id) => {
+  return $.ajax({
+    method: 'GET',
+    url: `api/pokemon/${id}`
+  });
+};
